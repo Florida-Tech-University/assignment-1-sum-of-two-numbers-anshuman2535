075a9e484ff30254d9a91d121daebf473cb7080a
anshuman2535 <143724193+anshuman2535@users.noreply.github.com>
2026-01-24 13:43:08 +0530

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int a = sc.nextINT();
-        int b = sc.nextINT();// TODO: Read two integers
-      
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
 
-        System.out.println(a+b);// TODO: Print their sum  using println
-      
+        System.out.println(a + b);
+
         scanner.close();
     }
 }
